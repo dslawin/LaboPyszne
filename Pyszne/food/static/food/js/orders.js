b5581c295b8f9a73cbacde0449e11ad7dccbf9ed
dslawin <dslawin00@gmail.com>
2021-12-24 00:14:05 +0100

    Naliczanie na listę zakupów

--- Pyszne/food/static/food/js/orders.js
+++ Pyszne/food/static/food/js/orders.js
@@ -1,5 +1,27 @@
 var pcart = document.querySelector('#pcart');
-var proral= document.querySelector('#ptotal');
+var ptotal= document.querySelector('#ptotal');
 
-pcart.innerHTML = '<li>Pizza1 Średnia 20.00 zł</li>';
-ptotal.innerHTML = 'Razem: 20.00 zł';
+
+
+function addPizza(pid){
+    pizzaId = '#piz' + pid;
+    var name = document.querySelector(pizzaId).innerHTML;
+    var radio ='pizza' + pid;
+    var pri = document.getElementsByName(radio);
+    var size, price;
+    if(pri[0].checked){
+        price = pri[0].value;
+        size = 'Średnia';
+    }
+    else{
+        price = pri[1].value;
+        size= 'Duża';
+    }
+    var orders = JSON.parse(localStorage.getItem('orders'));
+    var total = localStorage.getItem('total');
+
+
+    total = Number(total) + Number(price);
+    ptotal.innerHTML = 'Razem: ' + total + ' zł';
+    pcart.innerHTML += '<li>'+ name + ' ' + size + ': ' + price +' zł</li>';
+}
